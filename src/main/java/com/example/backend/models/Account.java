b262b565d06246e569977aeac65689e8d5a60296
huy nguyen <huynn@hivetech.vn>
2023-09-08 21:49:48 +0700

    test commit

---
+++ src/main/java/com/example/backend/models/Account.java
@@ -0,0 +1,29 @@
+package com.example.backend.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+public class Account {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idTaiKhoan;
+
+    private String username;
+
+    private String password;
+
+
+}
